39e24aa971857176d49538de48adff18aa0c8660
dan-nl <d_entous@yahoo.com>
2016-11-22 08:02:02 +0100

    corrected use of GERRIT_USER variable in clone statement

--- bin/clone-mediawiki.sh
+++ bin/clone-mediawiki.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 msgRule "INFO"
-msg "INFO" "clone mediawiki"
+msg "INFO" "clone MediaWiki core"
 
 
 if [ ! -d $MW_CORE_DIR ]; then
-	git clone -o gerrit ssh://GERRIT_USER@gerrit.wikimedia.org:29418/mediawiki/core $MW_CORE_DIR
+	git clone -o gerrit "ssh://$GERRIT_USER@gerrit.wikimedia.org:29418/mediawiki/core" $MW_CORE_DIR
 else
 	msg "INFO" "mediawiki directory already exists, no need to clone it"
 fi
